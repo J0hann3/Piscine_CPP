bb0aaa25a598dc6fb14fa74b781132d5a7748484
J0hann3 <jvigny@student.42.fr>
2023-11-28 15:48:35 +0100

    [IMPROVE] presentation cout

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 14:23:10 by jvigny            #+#    #+#             */
-/*   Updated: 2023/11/28 14:55:53 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/11/28 15:47:59 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,23 +28,27 @@ void Harl::complain( std::string level )
 
 void Harl::debug( void )
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- \
-ketchup burger. I really do!" << std::endl;
+ketchup burger. I really do!" << std::endl << std::endl;
 }
 
 void Harl::info( void )
 {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put \
-enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 }
 
 void Harl::warning( void )
 {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for \
-years whereas you started working here since last month." << std::endl;
+years whereas you started working here since last month." << std::endl << std::endl;
 }
 
 void Harl::error( void )
 {
-	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }

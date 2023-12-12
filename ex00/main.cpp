2c2f2f099219e97816dab956d6a12effeefd6eac
J0hann3 <jvigny@student.42.fr>
2023-12-12 13:08:57 +0100

    [ADD] test ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 17:24:33 by jvigny            #+#    #+#             */
-/*   Updated: 2023/11/26 17:35:43 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/12/12 13:06:55 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ int main()
 {
 	Zombie *z1 = newZombie("Zombie1");
 	z1->announce();
-
-	randomChump("Zonbie2");
+	Zombie *z2 = newZombie("Zombie2");
+	z2->announce();
 	delete z1;
+	delete z2;
+
+	randomChump("Zombie3");
+	randomChump("Zombie4");
 }

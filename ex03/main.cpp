2e1175a82de6102fd0adcb741678f77a035e7fbb
J0hann3 <jvigny@student.42.fr>
2023-12-17 19:43:38 +0100

    [ADD] test in main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 14:31:15 by jvigny            #+#    #+#             */
-/*   Updated: 2023/12/17 19:20:56 by jvigny           ###   ########.fr       */
+/*   Updated: 2023/12/17 19:42:31 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,29 +29,28 @@ int main()
 	src->learnMateria(new Cure());
 	ICharacter* me = new Character("me");
 	AMateria* tmp;
-	AMateria* hey;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
 	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	hey = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
 	me->equip(tmp);
 	ICharacter* bob = new Character("bob");
+
+	ICharacter* test = new Character((Character const &)*me);
+	tmp = src->createMateria("cure");
+	test->equip(tmp);
+	test->use(3, *bob);
+
 	me->use(4, *bob);
 	me->use(-1, *bob);
 	me->use(0, *bob);
 	me->use(1, *bob);
 	me->use(2, *bob);
-	me->unequip(3);
-	me->unequip(3);
-	delete hey;
 	me->use(3, *bob);
 	delete bob;
 	delete me;
 	delete src;
+	delete test;
 	return 0;
 }

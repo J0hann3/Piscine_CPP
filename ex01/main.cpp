6a57e611922d2c8afa55020063f9759d23f85167
J0hann3 <jvigny@student.42.fr>
2024-01-04 14:35:03 +0100

    [ADD] test in main ex00

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,12 +6,13 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 19:21:30 by jvigny            #+#    #+#             */
-/*   Updated: 2023/12/10 20:20:10 by jvigny           ###   ########.fr       */
+/*   Updated: 2024/01/04 14:34:30 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include <iostream>
 
 int main()
 {
@@ -19,21 +20,29 @@ int main()
 	ScavTrap Alex;
 	ScavTrap Alexandre("Alexandre");
 
-	Alexandre.guardGate();
+	std::cout << std::endl;
+
 	Alexandre.attack("Alex");
 	Alex.takeDamage(Alexandre.getAttackDamage());
 	Alex.beRepaired(10);
 
-	Quentin.attack("Alexandre");
-	Alexandre.takeDamage(Quentin.getAttackDamage());
+	std::cout << std::endl;
+	for (int i = 0; i < 52; i++)
+	{
+		Quentin.attack("Alexandre");
+		Alexandre.takeDamage(Quentin.getAttackDamage());
+	}
+	std::cout << std::endl;
 
-	Alexandre.beRepaired(1);
+	Quentin.beRepaired(1);
 
-	Quentin.attack("Alexandre");
-	Alexandre.takeDamage(Quentin.getAttackDamage());
+	Alex.attack("Quentin");
+	Quentin.takeDamage(Alex.getAttackDamage());
 
-	Alex.attack("Alexandre");
-	Alexandre.takeDamage(Alex.getAttackDamage());
+	Alex.attack("Quentin");
+	Quentin.takeDamage(Alex.getAttackDamage());
+	
+	std::cout << std::endl;
 
 	return 0;
 }

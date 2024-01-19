cc4c531468ba7d3d516d199f1f7b9affa1707f23
J0hann3 <johanne.vgn@gmail.com>
2024-01-19 19:53:40 +0100

    [FIX] template function to use algorithm

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,27 +6,24 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:45:05 by jvigny            #+#    #+#             */
-/*   Updated: 2024/01/19 19:26:38 by jvigny           ###   ########.fr       */
+/*   Updated: 2024/01/19 19:52:39 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 #include <iostream>
+#include <algorithm>
 
 template<typename T>
 void easyfind(T& container, int value)
 {
-	for (typename T::const_iterator it = container.begin(); it != container.end(); it++)
-	{
-		if (*it == value)
-		{
-			std::cout << "Find" << std::endl;
-			return;
-		}
-	}
-	std::cout << "Not Find" << std::endl;
-	return;
+	typename T::const_iterator it;
+	it = std::find(container.begin(), container.end(), value);
+	if (it != container.end())
+		std::cout << "Element find" << std::endl;
+	else
+		std::cout << "No element find" << std::endl;
 }
 
 #endif

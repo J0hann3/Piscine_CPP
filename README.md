5fa789f760337416a7232d97f2dfbb492ecbf4cf
J0hann3 <jvigny@student.42.fr>
2024-01-22 18:26:07 +0100

    [ADD] file for ex

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CPP_Module9
+
+>This document contains the exercises of Module 09 from C++ modules
+
+[](https://cdn.intra.42.fr/pdf/pdf/103376/en.subject.pdf)
---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 17:49:17 by jvigny            #+#    #+#             */
+/*   Updated: 2024/01/22 18:09:05 by jvigny           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+# include <map>
+
+class BitcoinExchange
+{
+private:
+	std::map<unsigned int, > _map;
+public:
+};
+
+#endif

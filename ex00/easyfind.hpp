70587d068be53d929b4b4b4ec53ad9743dfcc708
J0hann3 <jvigny@student.42.fr>
2024-02-12 12:18:25 +0100

    [ADD] second template for none const arguments

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:45:05 by jvigny            #+#    #+#             */
-/*   Updated: 2024/01/22 16:33:36 by jvigny           ###   ########.fr       */
+/*   Updated: 2024/02/12 12:16:47 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,37 @@
 #include <algorithm>
 
 template<typename T>
-void easyfind(T const & container, int value)
+int easyfind(T const & container, int const value)
 {
 	typename T::const_iterator it;
 	it = std::find(container.begin(), container.end(), value);
 	if (it != container.end())
+	{
 		std::cout << "Element find" << std::endl;
+		return true;
+	}
 	else
+	{
 		std::cout << "No element find" << std::endl;
+		return false;
+	}
+}
+
+template<typename T>
+int easyfind(T & container, int  value)
+{
+	typename T::iterator it;
+	it = std::find(container.begin(), container.end(), value);
+	if (it != container.end())
+	{
+		std::cout << "Element find" << std::endl;
+		return true;
+	}
+	else
+	{
+		std::cout << "No element find" << std::endl;
+		return false;
+	}
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 18:57:39 by jvigny            #+#    #+#             */
-/*   Updated: 2024/01/22 16:35:30 by jvigny           ###   ########.fr       */
+/*   Updated: 2024/02/12 12:17:12 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ int main()
 	std::vector<int> tab(10, 13);
 	easyfind(tab, 7);
 	easyfind(tab, 13);
+	const std::vector<int> tabConst(10, 13);
+	easyfind(tabConst, 7);
+	easyfind(tabConst, 13);
 
 	std::cout << "--- Deque ---" << std::endl;
 	std::deque<int> queue(tab.begin(), tab.end());
@@ -28,7 +31,7 @@ int main()
 	easyfind(queue, 7);
 
 	std::cout << "--- List ---" << std::endl;
-	std::list<int> lst(0, 5);
+	std::list<int> lst(0, 5); // ! list of size zero
 	easyfind(lst, 13);
 	easyfind(lst, 5);
 	std::list<int> lst1(5, 15);

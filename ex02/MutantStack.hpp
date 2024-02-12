1ee0a0652f3e75c47ff2d67847d104eef25cb13d
J0hann3 <jvigny@student.42.fr>
2024-02-12 12:14:03 +0100

    [FIX] iterator by commenting the class iterator and using c instead

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 19:20:12 by jvigny            #+#    #+#             */
-/*   Updated: 2024/01/24 17:48:22 by jvigny           ###   ########.fr       */
+/*   Updated: 2024/02/12 12:07:28 by jvigny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,67 +29,68 @@ class MutantStack : public std::stack<T, Container>
 	}
 	~MutantStack(){}
 
-	// using std::stack<T, Container>::c;
-	// typedef typename Container::iterator iterator;
+	typedef typename Container::iterator iterator;
 	
-	// iterator begin() { return c.begin(); }
-	// iterator end() { return c.end(); }
-	
-	class iterator {
-	private:
-		T* _ptr;
-	public:
-		iterator(): _ptr(NULL){}
-		iterator(T* ptr): _ptr(ptr){}
-		iterator(iterator const & copy): _ptr(copy._ptr){}
-		iterator & operator=(iterator const & assign){
-			_ptr = assign._ptr;
-			return *this;
-		}
-		~iterator(){}
-		iterator& operator++(){
-			_ptr++;
-			return *this;
-		}
-		iterator& operator++(int){
-			iterator &tmp = *this;
-			_ptr++;
-			return tmp;
-		}
-		iterator& operator--(){
-			_ptr--;
-			return *this;
-		}
-		iterator& operator--(int){
-			iterator &tmp = *this;
-			_ptr--;
-			return tmp;
-		}
-		T* operator->(){
-			return _ptr;
-		}
-		T& operator*(){
-			if (_ptr != NULL)
-				return *_ptr;
-			throw std::exception();
-		}
-		bool operator==(const iterator & other) const{
-			return (_ptr == other._ptr);
-		}
-		bool operator!=(const iterator & other) const{
-			return (_ptr != other._ptr);
-		} 
-	};
-	iterator begin(){
-		if (this->size() == 0)
-			return iterator();
-		return iterator((&this->top()) - this->size() + 1);
-	}
-	iterator end(){
-		if (this->size() == 0)
-			return iterator();
-		return iterator((&this->top()) + 1);
-	}
+	iterator begin() { return std::stack<T, Container>::c.begin(); }
+	iterator end() { return std::stack<T, Container>::c.end(); }
+
+	// * Create an iterator for the stack
+	// ! Doesn't work because stack isn't stored continuously
+	// class iterator {
+	// private:
+	// 	T* _ptr;
+	// public:
+	// 	iterator(): _ptr(NULL){}
+	// 	iterator(T* ptr): _ptr(ptr){}
+	// 	iterator(iterator const & copy): _ptr(copy._ptr){}
+	// 	iterator & operator=(iterator const & assign){
+	// 		_ptr = assign._ptr;
+	// 		return *this;
+	// 	}
+	// 	~iterator(){}
+	// 	iterator& operator++(){
+	// 		_ptr++;
+	// 		return *this;
+	// 	}
+	// 	iterator& operator++(int){
+	// 		iterator &tmp = *this;
+	// 		_ptr++;
+	// 		return tmp;
+	// 	}
+	// 	iterator& operator--(){
+	// 		_ptr--;
+	// 		return *this;
+	// 	}
+	// 	iterator& operator--(int){
+	// 		iterator &tmp = *this;
+	// 		_ptr--;
+	// 		return tmp;
+	// 	}
+	// 	T* operator->(){
+	// 		return _ptr;
+	// 	}
+	// 	T& operator*(){
+	// 		if (_ptr != NULL)
+	// 			return *_ptr;
+	// 		throw std::exception();
+	// 	}
+	// 	bool operator==(const iterator & other) const{
+	// 		return (_ptr == other._ptr);
+	// 	}
+	// 	bool operator!=(const iterator & other) const{
+	// 		return (_ptr != other._ptr);
+	// 	} 
+	// };
+	// iterator begin(){
+	// 	if (this->size() == 0)
+	// 		return iterator();
+	// 	return iterator((&this->top()) - this->size() + 1);
+	// }
+	// iterator end(){
+	// 	if (this->size() == 0)
+	// 		return iterator();
+	// 	return iterator((&this->top()) + 1);
+	// }
 };
 
 #endif
